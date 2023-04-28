1ff55a3b98bbb117a5f351fd3dfed3b94a42f453
Alberaan <43955907+Alberaan@users.noreply.github.com>
2023-04-28 15:33:50 +0200

    Add files via upload

--- tablas aleatorias.js
+++ tablas aleatorias.js
@@ -3,9 +3,55 @@
 
 cargarDatosDefault();
 const datos = cargarDatosNavegador();
+rellenarBotones();
 
 function cargarDatosDefault(){
   localStorage.setItem("datos_sistemas", jsonDatos);
+}
+
+function rellenarBotones(){
+  const containerBotones = document.getElementById('botones');
+  cont = 0;
+  var row = document.createElement('div');
+  row.className = 'row mt-3';
+
+  for (const sistema of datos.sistemas){
+    for (const fichero of sistema.ficheros){
+      btn = crearBoton(sistema, fichero);
+      var col1 = document.createElement('div');
+      col1.className = 'col-md-3';
+      col1.appendChild(btn);
+      
+      //containerBotones.appendChild(btn);
+
+      if(cont === 0){
+        containerBotones.appendChild(row);
+        var row = document.createElement('div');
+        row.className = 'row mt-3';
+      }
+      row.appendChild(col1);
+      cont++;
+      if (cont >= 4){
+        cont = 0;
+      }
+    }
+  }
+}
+
+function crearBoton(sistema, fichero){
+  var btn = document.createElement('button');
+  btn.className = 'btn btn-primary btn-block';
+  btn.innerHTML = fichero.nombre.replace(".txt", "").replaceAll("_", " ");
+  btn.style = "white-space:normal;display:inline-flex";
+
+  btn.addEventListener('click', function() {
+    botonClickado(sistema.nombre, fichero.nombre);
+  });
+  return btn;
+}
+
+function botonClickado(sistema, fichero){
+  obtenerElementoAleatorioBoton(sistema, fichero);
 }
 
 function cargarDatosNavegador(){
@@ -75,7 +121,6 @@ function cargarSistemas(){
 
 function generarTablaAleatoria(tabla) {
   const numeroAleatorio = Math.floor(Math.random() * tabla.length);
-  console.log(tabla.length + " " + numeroAleatorio)
   return tabla[numeroAleatorio];
 }
 
@@ -84,6 +129,30 @@ function obtenerElementoAleatorio(){
   const resultadoTextarea = document.getElementById('resultado');
 
   elementos = contenidoTextarea.value.split("\n");
+  elementos = elementos.filter(item => item);
+  elementoAleatorio = generarTablaAleatoria(elementos);
+
+  stringAdevolver = resultadoTextarea.value + elementoAleatorio + "\n";
+
+  resultadoTextarea.value = stringAdevolver;
+  var textarea = document.getElementById("resultado");
+  textarea.scrollTop = textarea.scrollHeight;
+}
+
+function obtenerElementoAleatorioBoton(sistemaSeleccionado, ficheroSeleccionado){
+  for (const sistema of datos.sistemas){
+    if (sistema.nombre == sistemaSeleccionado){
+      for (const fichero of sistema.ficheros){
+        if (fichero.nombre == ficheroSeleccionado){
+          contenidoTabla = fichero.contenido;
+        }
+      }
+    }
+  }
+
+  const resultadoTextarea = document.getElementById('resultado');
+
+  elementos = contenidoTabla.split("\n");
   elementos = elementos.filter(item => item);
   elementoAleatorio = generarTablaAleatoria(elementos);
 
