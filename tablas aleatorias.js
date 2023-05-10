99ea396a735bf268d097216b50815f330687a126
Alberaan <43955907+Alberaan@users.noreply.github.com>
2023-05-10 13:42:38 +0200

    Add files via upload

--- tablas aleatorias.js
+++ tablas aleatorias.js
@@ -118,7 +118,7 @@ function cargarDatosOnLoad(){
 
 function cargarDatosDefault(){
   jsonDatos = cargarJsonExternoDefault();
-  localStorage.setItem("datos_sistemas", jsonDatos.stringify();
+  localStorage.setItem("datos_sistemas", jsonDatos.stringify());
   datos = cargarDatosNavegador();
   cargarDatos();
   rellenarBotones();
