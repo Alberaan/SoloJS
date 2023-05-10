1a25e80a9c1174995cacf7d985aae2b68b3f9b69
Alberaan <43955907+Alberaan@users.noreply.github.com>
2023-05-10 13:34:12 +0200

    Add files via upload

--- tablas aleatorias.js
+++ tablas aleatorias.js
@@ -131,7 +131,7 @@ function cargarJsonExternoDefault(){
 
   if (xhr.readyState === 4 && xhr.status === 200) {
     var data = JSON.parse(xhr.responseText);
-    return data;
+    return data.stringify();
     // trabaja con el objeto JavaScript aquí
   } else {
     console.error('No se pudo leer el archivo JSON.');
